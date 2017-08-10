a154fcd8848f8c71a747f8f1972374c15816e01b
Alexander Pope <alex@pope-industries.com>
2017-08-10 14:06:17 +0200

    clean up

--- test/fixtures/test-client.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const ReactDom = require('react-dom');
-const testComponent = require('./testComponent');
-
-const comp = testComponent.create();
-
-ReactDom.render(comp({ label: 'click here!' }), document.getElementById('container'));
