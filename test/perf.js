814ea26d394701027dd1830824fce5e67e990706
Alexander Pope <alex@pope-industries.com>
2017-08-10 13:44:33 +0200

    update perf test

--- test/perf.js
+++ test/perf.js
@@ -1,25 +1,23 @@
 'use strict';
 
-const benchmark = require('benchmark')
-  , component = require('../src/index')
-  , reactDom = require('react-dom/server')
+const { el, define } = require('../src/index');
+const benchmark = require('benchmark');
+const reactDOM = require('react-dom/server');
 
-  , BREADTH = 11
-  , DEPTH = 4
+const BREADTH = 11;
+const DEPTH = 4;
 
-  , el = component.el
-  , suite = new benchmark.Suite();
+const suite = new benchmark.Suite();
 
-const comp = component.create({
-  render (props) {
-    const breadth = props.breadth
-      , depth = props.depth;
+const comp = define({
+  render(props) {
+    const { breadth, depth } = props;
 
     if (depth <= 0) {
       return el('div', null, 'abcdefghi');
     }
 
-    let children = [];
+    const children = [];
 
     for (let i = 0; i < breadth; i++) {
       children.push(comp({ key: i, depth: depth - 1, breadth }));
@@ -27,24 +25,24 @@ const comp = component.create({
     return el('div', { children, onClick: this.onClick });
   },
 
-  onClick () {
+  onClick() {
     console.log('click');
   }
 });
 
-function render () {
-  return reactDom.renderToString(comp({ depth: DEPTH, breadth: BREADTH }));
+function render() {
+  return reactDOM.renderToString(el(comp, { depth: DEPTH, breadth: BREADTH }));
 }
 
 suite
   .add({ name: 'comp', fn: render })
-  .on('complete', function () {
-      for (let i = 0; i < this.length; i++) {
-        const benchmark = this[i];
+  .on('complete', function() {
+    for (let i = 0; i < this.length; i++) {
+      const benchmark = this[i];
 
-        console.log(benchmark.name);
-        console.log(`Mean:    ${Math.round(benchmark.stats.mean * 1000)} ms`);
-        console.log(`Std Dev: ${Math.round(benchmark.stats.deviation * 1000)} ms\n`);
-      }
-    })
+      console.log(benchmark.name);
+      console.log(`Mean:    ${Math.round(benchmark.stats.mean * 1000)} ms`);
+      console.log(`Std Dev: ${Math.round(benchmark.stats.deviation * 1000)} ms\n`);
+    }
+  })
   .run({ async: true });
