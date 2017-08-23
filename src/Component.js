aac3e8aceead8e0a840d302a51f131e390a40202
Alexander Pope <alex@pope-industries.com>
2017-08-23 09:27:11 +0200

    remove transition behaviour

--- src/Component.js
+++ src/Component.js
@@ -2,10 +2,6 @@
 
 const { Component: PreactComponent } = require('preact');
 const assign = require('object-assign');
-const clock = require('@yr/clock');
-
-const DEFAULT_TRANSITION_DURATION = 250;
-const TIMEOUT = 20;
 
 class Component extends PreactComponent {
   /**
@@ -16,9 +12,6 @@ class Component extends PreactComponent {
   constructor(props, context) {
     super(props, context);
 
-    this.__timerID = 0;
-    this.__transitionDuration =
-      'getTransitionDuration' in this ? this.getTransitionDuration() : DEFAULT_TRANSITION_DURATION;
     // Set up initial state
     this.state = assign({}, this.__state);
     // Autobind mixin methods
@@ -40,74 +33,6 @@ class Component extends PreactComponent {
    */
   render() {
     return this.__render(this.props, this.state, this.context);
-  }
-
-  /**
-   * Determine if component should transition based on 'nextProps' or 'nextState'
-   * @param {Object} nextProps
-   * @param {Object} nextState
-   * @returns {Boolean}
-   */
-  shouldComponentTransition(nextProps, nextState) {
-    return false;
-  }
-
-  /**
-   * Update 'state' for transition
-   * @param {Object} state
-   */
-  willTransition(state) {
-    if (this.__timerID) {
-      clock.cancel(this.__timerID);
-    }
-
-    // Generally a bad idea...
-    state.visibility = !state.visibility ? 1 : 2;
-
-    // frame/immediate doesn't leave enough time for redraw between states
-    this.__timerID = clock.timeout(TIMEOUT, () => {
-      this.isTransitioning();
-    });
-  }
-
-  /**
-   * Trigger transition state change
-   */
-  isTransitioning() {
-    this.setState({
-      visibility: this.state.visibility == 1 ? 2 : 1
-    });
-
-    this.__timerID = clock.timeout(this.__transitionDuration, () => {
-      this.didTransition();
-    });
-  }
-
-  /**
-   * Trigger transition state change
-   */
-  didTransition() {
-    this.__timerID = 0;
-
-    this.setState({
-      visibility: this.state.visibility == 2 ? 3 : 0
-    });
-  }
-
-  /**
-   * React: componentWillUnmount
-   */
-  componentWillUnmount() {
-    // Reset
-    if (this.state && this.state.visibility) {
-      this.state.visibility = 0;
-    }
-    if (this.__timerID) {
-      clock.cancel(this.__timerID);
-    }
-    if (this.__componentWillUnmount) {
-      this.__componentWillUnmount();
-    }
   }
 }
 
